e3a70a67099971eec3db66e2adb65fa8bf70eb70
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-07 07:23:56 +0300

    startup changed

--- IOC.web/Models/DateService.cs
+++ IOC.web/Models/DateService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace IOC.web.Models
 {
     public class DateService : ISingletonDateService, IScopedDateService, ITransientDateService
     {
+        private readonly ILogger<DateService> _logger;
+        public DateService(ILogger<DateService> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning("DATESERVİCE CONSTRUCTORA GİRDİ");
+        }
         public DateTime GetDateTime { get; } = DateTime.Now;
     }
 
 
 }
+ 
